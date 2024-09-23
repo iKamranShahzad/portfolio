66904b3937201375e06d86d5461ac46c1a7d3671
iKamranShahzad <mailkamranshahzad@gmail.com>
2024-09-23 20:51:31 +0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# portfolio
-My Personal Portfolio Website
+# Portfolio Website
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <div className="flex flex-col min-h-screen bg-gray-950 text-white">
+        <main className="flex-grow flex items-center justify-center">
+          <div className="text-center">
+            <h1 className="text-4xl font-bold mb-4">My Portfolio</h1>
+          </div>
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default App;
